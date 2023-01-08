961503f7b85c686fa80d45afdd875bf2359504ad
Goose <untitledgeese@gmail.com>
2023-01-07 19:13:25 -0600

    Add /delhome & /delwarp completions

--- src/main/java/me/aroze/tarnaisbad/listeners/TabCompleteListener.kt
+++ src/main/java/me/aroze/tarnaisbad/listeners/TabCompleteListener.kt
@@ -16,32 +16,42 @@ object TabCompleteListener : Listener {
 		val chunks = e.buffer.split(Regex("\\s+"))
 		val args = chunks.subList(1, chunks.size)
 		val last = args.last()
+		val name = chunks[0].substring(1).replace("^tarnaisbad:", "")
 
+		e.completions = when (name) {
+			"delhome" -> getHomeCompletions(sender, last)
+			"home" -> getHomeCompletions(sender, last)
+			"delwarp" -> getWarpCompletions(last)
+			"warp" -> getWarpCompletions(last)
+			else -> return
+		}
+	}
+
+	fun getHomeCompletions(owner: Player, last: String): List<String> {
 		val completions = mutableListOf<String>()
+		val rs = SQL.query("SELECT name FROM homes WHERE owner = ?", owner.uniqueId.toString())
 
-		when (chunks[0].substring(1)) {
-			"home" -> {
-				val rs = SQL.query("SELECT name FROM homes WHERE owner = ?", sender.uniqueId.toString())
+		while (rs.next()) {
+			val name = rs.getString("name")
 
-				while (rs.next()) {
-					val name = rs.getString("name")
-
-					if (name.contains(last, true))
-						completions.add(name)
-				}
-			}
-			"warp" -> {
-				val rs = SQL.query("SELECT name FROM warps")
-
-				while (rs.next()) {
-					val name = rs.getString("name")
-
-					if (name.contains(last, true))
-						completions.add(name)
-				}
-			}
+			if (name.contains(last, true))
+				completions.add(name)
 		}
 
-		e.completions = completions
+		return completions
+	}
+
+	fun getWarpCompletions(last: String): List<String> {
+		val completions = mutableListOf<String>()
+		val rs = SQL.query("SELECT name FROM warps")
+
+		while (rs.next()) {
+			val name = rs.getString("name")
+
+			if (name.contains(last, true))
+				completions.add(name)
+		}
+
+		return completions
 	}
 }
